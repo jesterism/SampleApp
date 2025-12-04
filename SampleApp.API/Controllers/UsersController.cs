d02e384ff36bb4ce220bdb6b0583e473eb8fd610
prikol <roflaninternetov@proton.me>
2025-12-04 17:11:47 +0300

    Реализация статус-кода 201 в методе контроллера для создания пользователя

--- SampleApp.API/Controllers/UsersController.cs
+++ SampleApp.API/Controllers/UsersController.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using SampleApp.API.Entities;
+using SampleApp.API.Interfaces;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace SampleApp.API.Controllers
